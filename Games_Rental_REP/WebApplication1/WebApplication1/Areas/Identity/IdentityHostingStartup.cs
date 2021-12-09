8f1fa640017683715d02ae74d029c2ec5733a292
Tobias Høj-Kristensen <tobiaskristensen.thj@gmail.com>
2021-12-09 09:14:19 +0100

    Første del med login er oprettet på siden.

---
+++ Games_Rental_REP/WebApplication1/WebApplication1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Areas.Identity.Data;
+using WebApplication1.Data;
+
+[assembly: HostingStartup(typeof(WebApplication1.Areas.Identity.IdentityHostingStartup))]
+namespace WebApplication1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
